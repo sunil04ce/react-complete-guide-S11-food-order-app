1c29fe687edf35b7f864954429277a26f9282ce1
sunil parmar <sunil04ce@gmail.com>
2023-02-16 22:30:55 +0530

    added CartProvider context

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -4,20 +4,20 @@ import classes from './Modal.module.css';
 
 
 const Backdrop = props => {
-    return <div className={classes.Backdrop}></div>
+    return (<div className={classes.backdrop} onClick={props.onClose}></div>);
 };
 
 const ModalOverlay = props => {
-    return <div className={classes.modal}>
+    return (<div className={classes.modal}>
         <div className={classes.content}>{props.children}</div>
-    </div>
+    </div>);
 };
 
 const portalElement = document.getElementById('overlays');
 
 const Modal = props => {
     return <Fragment>
-        {ReactDOM.createPortal(<Backdrop />, portalElement)}
+        {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
         {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
     </Fragment>
 };
